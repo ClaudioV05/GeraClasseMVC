7dac96598df368ed8a67b8525188c4cfc0454585
Claudio Ventura <104768019+ClaudioV05@users.noreply.github.com>
2022-12-31 18:16:15 -0300

    #0021 Ajuste geral.

--- GcMvcV5/01-Apresentacao/GeraClasseMvc.Web/Startup.cs
+++ GcMvcV5/01-Apresentacao/GeraClasseMvc.Web/Startup.cs
@@ -1,18 +1,11 @@
-using GeraClasseMvc.Web.Controllers.Principal;
 using GeraClasseMvc.Web.Services;
-using GeraClasseMvc.Api.Services;
+using GeraClasseMvc.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using GeraClasseMvc.Api.Services.Interfaces;
-using GeraClasseMvc.Web.Services.Interfaces;
 
 namespace GeraClasseMvc.Web
 {
