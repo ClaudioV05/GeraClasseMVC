24cdec7a7f34b53b489b48fdb6105e55d2fb3f9d
Claudio Ventura <104768019+ClaudioV05@users.noreply.github.com>
2022-12-31 18:11:14 -0300

    #0021 Ajuste geral - Documentação das Interfaces.

--- GcMvcV5/02-Aplicacao/GeraClasseMvc.Api/Services/Interfaces/IMetodosGenericos.cs
+++ GcMvcV5/02-Aplicacao/GeraClasseMvc.Api/Services/Interfaces/IMetodosGenericos.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 
 namespace GeraClasseMvc.Api.Services.Interfaces
 {
+    /// <summary>
+    /// Interface IMetodosGenericos.
+    /// </summary>
     public interface IMetodosGenericos
     {
+        /// <summary>
+        /// Listagem do Bancos de Dados.
+        /// </summary>
         List<string> ListagemBancosDeDados();
+
+        /// <summary>
+        /// Listagem Estilo do Formulário.
+        /// </summary>
         List<string> ListagemEstiloFormulario();
+
+        /// <summary>
+        /// Listagem IDE de Desenvolvimento.
+        /// </summary>
         List<string> ListagemIdeDesenvolvimento();
+
+        /// <summary>
+        /// Método para Retornar o Tipo do Banco de Dados.
+        /// </summary>
         TipoBancodeDados RetornaTipoBancoDeDados(string bancodedados);
     }
 }
