22a6cdf4d160021941d3d87fffb3556bc15d163a
Claudio Ventura <104768019+ClaudioV05@users.noreply.github.com>
2022-12-31 18:13:51 -0300

    #0021 Ajuste geral - Documentação das Interfaces.

--- GcMvcV5/02-Aplicacao/GeraClasseMvc.Api/Services/Interfaces/IUtilsMvcApiPrincipal.cs
+++ GcMvcV5/02-Aplicacao/GeraClasseMvc.Api/Services/Interfaces/IUtilsMvcApiPrincipal.cs
@@ -3,8 +3,15 @@ using System.Threading.Tasks;
 
 namespace GeraClasseMvc.Api.Services.Interfaces
 {
+    /// <summary>
+    /// Interface IUtilsMvcApiPrincipal.
+    /// </summary>
+
     public interface IUtilsMvcApiPrincipal
     {
+        /// <summary>
+        /// Método para Retornar a Descrição das Tabelas.
+        /// </summary>
         Task<GeraClasse> RetornaDescricaoTabelas(string baseURL, GeraClasse metadata);
     }
 }
